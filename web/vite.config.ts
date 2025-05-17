231a085a7ef01bb21aef2caf19d7b07efdc1a85c
Rav <64511217+arsatija@users.noreply.github.com>
2025-05-16 22:59:01 -0400

    Install shadcn

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'build',
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    base: "./",
+    build: {
+        outDir: "build",
+    },
 });
